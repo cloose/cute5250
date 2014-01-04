21cc75aaf7315f8f682997d45076b4a40ca019d9
Christian Loose <christian.loose@hamburg.de>
2014-01-04 22:38:08 +0100

    Initial version of the cute5250 application

---
+++ src/mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+#include <QNetworkProxyFactory>
+
+#include <telnetclient.h>
+#include <terminalemulation.h>
+using q5250::TelnetClient;
+using q5250::TerminalEmulation;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    client(new TelnetClient(this)),
+    emulation(0)
+{
+    ui->setupUi(this);
+
+    emulation = new TerminalEmulation(ui->terminalWidget);
+
+    connect(client, &TelnetClient::dataReceived,
+            emulation, &TerminalEmulation::dataReceived);
+
+    client->setTerminalType("IBM-3477-FC");
+    client->connectToHost("pub1.rzkh.de");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
