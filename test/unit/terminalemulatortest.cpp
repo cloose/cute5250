0b73fca19295799b17babd0de243e8ebd9cc48ef
Christian Loose <christian.loose@hamburg.de>
2014-02-11 11:42:44 +0100

    TerminalEmulator set display buffer to received address

--- test/unit/terminalemulatortest.cpp
+++ test/unit/terminalemulatortest.cpp
@@ -28,11 +28,15 @@ using namespace testing;
 
 #include <QByteArray>
 
+#include <generaldatastream.h>
+using namespace q5250;
+
 class DisplayBuffer
 {
 public:
     virtual ~DisplayBuffer() {}
     virtual void setSize(int columns, int rows) = 0;
+    virtual void setBufferAddress(int column, int row) = 0;
 };
 
 
@@ -40,6 +44,7 @@ class DisplayBufferMock : public DisplayBuffer
 {
 public:
     MOCK_METHOD2(setSize, void(int, int));
+    MOCK_METHOD2(setBufferAddress, void(int, int));
 };
 
 class TerminalEmulator
@@ -49,23 +54,82 @@ public:
 
     void dataReceived(const QByteArray &data)
     {
-        displayBuffer->setSize(80, 25);
+        GeneralDataStream stream(data);
+
+        while (!stream.atEnd()) {
+            unsigned char byte = stream.readByte();
+
+            if (byte == 0x04 /*ESC*/) {
+                byte = stream.readByte();
+
+                switch (byte) {
+                case 0x11 /*WRITE TO DISPLAY*/:
+                    {
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        stream.readByte();
+                        unsigned char row = stream.readByte();
+                        unsigned char column = stream.readByte();
+                        displayBuffer->setBufferAddress(column, row);
+                    }
+                    break;
+                case 0x40 /*CLEAR UNIT*/:
+                    displayBuffer->setSize(80, 25);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
     }
 
 private:
     DisplayBuffer *displayBuffer;
 };
 
-TEST(ATerminalEmulator, setSizeOfDisplayOnClearUnit)
+class ATerminalEmulator : public Test
+{
+public:
+    static const char ESC = 0x04;
+    static const char ClearUnitCommand = 0x40;
+    static const char WriteToDisplayCommand = 0x11;
+    static const char StartOfHeaderOrder = 0x01;
+    static const char SetBufferAddressOrder = 0x11;
+
+    QByteArray createGdsHeaderWithLength(char length)
+    {
+        char fullLength = 0x0a + length;
+        const char gdsHeader[] { 0x00, fullLength, 0x12, (char)0xa0, 0x00, 0x00, 0x04, 0x00, 0x00, 0x03 };
+        return QByteArray(gdsHeader, 10);
+    }
+};
+
+TEST_F(ATerminalEmulator, setDisplayBufferToDefaultSizeOnReceivingClearUnit)
 {
     DisplayBufferMock displayBuffer;
     TerminalEmulator terminal;
     terminal.setDisplayBuffer(&displayBuffer);
     EXPECT_CALL(displayBuffer, setSize(80, 25));
-    const char gdsHeader[] { 0x00, 0x0c, 0x12, (char)0xa0, 0x00, 0x00, 0x04, 0x00, 0x00, 0x03 };
-    QByteArray data = QByteArray::fromRawData(gdsHeader, 10);
-    data += 0x04;       // ESC
-    data += 0x40;       // CU
+    const char streamData[]{ESC, ClearUnitCommand};
+    QByteArray data = createGdsHeaderWithLength(2) + QByteArray::fromRawData(streamData, 2);
+
+    terminal.dataReceived(data);
+}
+
+TEST_F(ATerminalEmulator, setDisplayBufferToReceivedAddress)
+{
+    DisplayBufferMock displayBuffer;
+    TerminalEmulator terminal;
+    terminal.setDisplayBuffer(&displayBuffer);
+    EXPECT_CALL(displayBuffer, setBufferAddress(5, 2));
+    const char streamData[]{ESC, WriteToDisplayCommand, 0x00, 0x18, StartOfHeaderOrder, 0x04, 0x00, 0x00, 0x00, 0x00, SetBufferAddressOrder, 0x02, 0x05};
+    QByteArray data = createGdsHeaderWithLength(13) + QByteArray::fromRawData(streamData, 13);
 
     terminal.dataReceived(data);
 }
